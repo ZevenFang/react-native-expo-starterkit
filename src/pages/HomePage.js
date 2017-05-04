76196cfd30d4ca88bfb59df95b16ced365a813b9
zeven <fangfan1997@126.com>
2017-05-04 19:35:13 +0800

    Title

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,16 @@ import {
 } from 'react-native';
 import {connect} from 'dva/mobile';
 
-/*class HomePage extends React.Component {
+class HomePage extends React.Component {
+
+  static route = {
+    navigationBar: {
+      backgroundColor: '#333',
+      tintColor: 'white',
+      title: 'Home'
+    }
+  };
+
   render() {
     const { dispatch, count } = this.props;
     return (
@@ -24,32 +33,14 @@ import {connect} from 'dva/mobile';
       </View>
     );
   }
-}*/
-
-const HomePage = connect(({ count }) => ({ count }))((props) => {
-  const { dispatch, count } = props;
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Count: { count }
-      </Text>
-      <TouchableHighlight onPress={() => { dispatch({ type: 'count/add' }) }}>
-        <Text>Add</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => { dispatch({ type: 'count/addDelay' }) }}>
-        <Text>Delay Add</Text>
-      </TouchableHighlight>
-    </View>
-  );
-});
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
 });
 
-export default HomePage;
+export default connect(({ count }) => ({ count }))(HomePage);
