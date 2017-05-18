49337a4cb23ce6d8c36a51b16e7214ee6a075f35
zeven <fangfan1997@126.com>
2017-05-18 20:44:48 +0800

    Todos TabNavigation

--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Container, Content, View, Text} from 'native-base';
+import {Container, Content, View, Text, Icon} from 'native-base';
 import {connect} from 'dva/mobile';
+import {TabNavigation, TabNavigationItem as TabItem} from '@expo/ex-navigation';
 
 class TodosPage extends React.Component {
 
@@ -12,6 +13,38 @@ class TodosPage extends React.Component {
 
   render() {
 
+    return (
+      <TabNavigation
+        id="main"
+        navigatorUID="main"
+        initialTab="home">
+        <TabItem
+          id="home"
+          selectedStyle={styles.selectedTab}
+          renderIcon={(isSelected) => <Icon name="list" style={{color: isSelected?"white":"gray"}} /> }>
+          <TodosList/>
+        </TabItem>
+        <TabItem
+          id="posts"
+          selectedStyle={styles.selectedTab}
+          renderIcon={(isSelected) => <Icon name="time" style={{color: isSelected?"white":"lightblue"}} /> }>
+          <TodosList/>
+        </TabItem>
+        <TabItem
+          id="profile"
+          selectedStyle={styles.selectedTab}
+          renderIcon={(isSelected) => <Icon name="checkmark-circle" style={{color: isSelected?"white":"lightgreen"}} /> }>
+          <TodosList/>
+        </TabItem>
+      </TabNavigation>
+    )
+  }
+
+}
+
+class TodosList extends React.Component {
+
+  render() {
     return (
       <Container>
         <Content contentContainerStyle={styles.container}>
@@ -30,6 +63,9 @@ const styles = {
     flex: 1,
     justifyContent: 'center'
   },
+  selectedTab: {
+    backgroundColor: '#AAA'
+  }
 };
 
 export default connect(({todos})=>({todos}))(TodosPage);
