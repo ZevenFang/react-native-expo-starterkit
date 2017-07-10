bd151c37e29963ef9c4bc8754b96bece3f8c75ce
zeven <fangfan1997@126.com>
2017-07-10 09:23:40 +0800

    fix expo load font on android

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Expo from 'expo';
+import Expo, {AppLoading} from 'expo';
 import { DrawerNavigator } from 'react-navigation';
 import './pages/HomePage';
 import router from './router';
@@ -16,14 +16,22 @@ const Drawer = DrawerNavigator(
 
 class App extends React.Component {
 
+  state = {
+    isReady: false
+  };
+
   async componentWillMount() {
     await Expo.Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
     });
+    this.setState({isReady: true});
   }
 
   render(){
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
     return(
       <Drawer/>
     )
